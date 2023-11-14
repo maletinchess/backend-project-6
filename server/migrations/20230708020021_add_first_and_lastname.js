190af882504987804096220f6cde21154cd80cec
maletinchess <maletinchess2017@gmail.com>
2023-11-14 20:23:01 +0700

    remove promise from migration

--- server/migrations/20230708020021_add_first_and_lastname.js
+++ server/migrations/20230708020021_add_first_and_lastname.js
@@ -1,11 +1,11 @@
-export const up  = async (knex) => {
-  await knex.schema.table('users', (table) => {
+export const up  = (knex) => {
+  knex.schema.table('users', (table) => {
     table.string('first_name');
     table.string('last_name');
   });
 };
 
-export const down = async (knex) => await knex.schema.table('users', (table) => {
+export const down = (knex) => knex.schema.table('users', (table) => {
     table.dropColumn('first_name');
     table.dropColumn('last_name');
   });
